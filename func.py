6b900613551e9b711ec64fdc460861beb38a215d
Ebere <eiweala@live.com>
2020-11-25 10:21:07 +0800

    Create func.py

---
+++ func.py
@@ -0,0 +1,69 @@
+# Code practice from codewars
+# The aim of this exercise to learn the way python.
+# May the Python be with you!!!
+
+# Prompt:
+# - Write a function called addList that accepts any quantity of numbers as arguments, adds them together and returns the resulting sum.
+# - Assume all parameters will be numbers.
+# - If called with no arguments, return 0 (zero).
+# Examples:
+# add(1) //=> 1
+# add(1,50,1.23) //=> 52.23
+# add(7,-12) //=> -5
+
+
+def add():
+    pass
+
+
+# Sum digits
+# sum_digits(10)  # returns 1
+# sum_digits(101) # returns 2
+# sum_digits(-32) # returns 5
+def sum_digits(number):
+    pass
+
+
+# iq_test(numbers)
+# iq_test('2 4 7 8 10') returns 3 which is the position of the odd number
+# print(iq_test("1 2 2") returns 1 as 1 is the odd number
+
+
+def iq_test(string_data):
+    pass
+
+
+# takes a string and returns the values below
+# accum('abcd') returns  A-Bb-Ccc-Dddd
+# accum("ZpglnRxqenU") returns Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu
+def accum(s):
+    pass
+
+
+# Count the bits
+# count_bits(0) returns 0
+# count_bits(7) binary for 7 is 111 and the bits are 3 (counting the number of 1's). Answer should be 3.
+# count_bits(10) binary for 10 is 1010 and the bits are 2(counting the number of 1). Answer should be 2
+def count_bits(n):
+    pass
+
+
+count_bits(0)  # 0
+count_bits(7)  # 3
+count_bits(10)  # 2
+
+
+# Likes
+# likes([]) # must be "no one likes this"
+# likes(["Peter"]) # must be "Peter likes this"
+# likes(["Jacob", "Alex"]) # must be "Jacob and Alex like this"
+# likes(["Max", "John", "Mark"]) # must be "Max, John and Mark like this"
+# likes(["Alex", "Jacob", "Mark", "Max"]) # must be "Alex, Jacob and 2 others like this"
+def likes(names):
+    #your code here
+    pass
+
+
+###--------------------######
+# ------Champions ONLY------#
+###-------------------#######
